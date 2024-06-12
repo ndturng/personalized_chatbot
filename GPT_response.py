7851fd758e0906d272f5d17f9fb5f716bb17ff03
Nguyen Duong Trung <103947618+ndturng@users.noreply.github.com>
2024-06-12 07:23:14 +0000

    handle errors

--- GPT_response.py
+++ GPT_response.py
@@ -1,3 +1,4 @@
+import logging
 from dotenv import load_dotenv
 from langchain.prompts import ChatPromptTemplate
 from langchain_openai import ChatOpenAI
@@ -7,6 +8,9 @@ from data.query import query_similar_documents
 # Get OpenAI API key from the .env file
 load_dotenv()
 
+# Initialize logging
+logging.basicConfig(level=logging.WARNING)
+logger = logging.getLogger(__name__)
 
 PROMPT_TEMPLATE = """
 Answer the question based only on the following context:
@@ -20,9 +24,17 @@ Answer the question based on the above context: {question}
 
 
 def generate_response_from_results(query_result, query_text):
+    # Handle the case when there is no result
     if query_result is None:
-        return None, None
+        response_text = "Sorry, looks like the information you are looking for is not available."
+        # If there is no result, the query function should fail, warning to check the query function and the database
+        logger.warning("Query result is None. There might be an issue with the query function or the database.")
+        return response_text, None
 
+    # Handle the case when the best match is not good enough, tell the user try again with more details
+    if query_result[0].metadata.score < 0.5:
+        response_text = "Sorry, it seems that is not enough information in your request, please provide more details or try another question."
+        return response_text, None
     # Preparing the context text from the search results
     context_text = "\n\n---\n\n".join(
         [obj.properties["content"] for obj in query_result]
--- data/query.py
+++ data/query.py
@@ -1,3 +1,4 @@
+import logging
 import sys
 
 import weaviate
@@ -5,6 +6,8 @@ from weaviate.classes.init import AdditionalConfig, Timeout
 from weaviate.classes.query import MetadataQuery
 from weaviate.connect import ConnectionParams
 
+logging.basicConfig(level=logging.WARNING)
+logger = logging.getLogger(__name__)
 # Constants
 URL = "http://localhost:8080"
 
@@ -18,7 +21,7 @@ def query_similar_documents(query, top_k=5):
             http_host="localhost",
             http_port="8080",
             http_secure=False,
-            grpc_host="localhost",
+            grpc_host="localhost",  # A mine
             grpc_port="50051",
             grpc_secure=False,
         ),
@@ -26,8 +29,13 @@ def query_similar_documents(query, top_k=5):
             timeout=Timeout(init=2, query=45, insert=120),  # Values in seconds
         ),
     )
+    # Try to connect to the client, if it fails, close the client and raise the exception
+    try:
+        client.connect()
 
-    client.connect()
+    except Exception:
+        logger.error("Failed to connect to the Weaviate client", exc_info=False)
+        return None  # Optionally return an empty list or handle as needed
 
     try:
         publications = client.collections.get("Document")
@@ -36,24 +44,23 @@ def query_similar_documents(query, top_k=5):
             limit=top_k,
             return_metadata=MetadataQuery(
                 certainty=True,
-                distance=True, 
+                distance=True,
                 # explain_score=True,
                 # is_consistent=True,
                 # rerank=True, this require to add a module
                 score=True,
-            )
+            ),
         )
+
+    except Exception as e:
+        logger.error(
+            "Query failed", exc_info=False
+        )  # exc_info=True for debugging
+        raise e
+
     finally:
         client.close()
 
-    print(f"Query text: {query}\n")
-    print(f"Top {top_k} similar documents:")
-    for obj in response.objects:
-        
-        # print(f"UUID: {obj.properties['uuid']}")
-        print(f"Source: {obj.properties['source']}")
-        print(f"Content: {obj.properties['content']}\n")
-    
     return response.objects
 
 
@@ -64,7 +71,17 @@ if __name__ == "__main__":
     #     sys.exit(1)
 
     # query = sys.argv[1]
-    
+
     top_k = int(sys.argv[2]) if len(sys.argv) > 2 else 5
 
-    query_similar_documents(query_text, top_k)
+    results = query_similar_documents(query_text, top_k)
+
+    if results is None:
+        print("No results found.")
+        sys.exit(1)  # Why used sys.exit(1) here?
+
+    print(f"Query text: {query_text}\n")
+    print(f"Top {top_k} similar documents:")
+    for obj in results:
+        print(f"Source: {obj.properties['source']}")
+        print(f"Content: {obj.properties['content']}\n")
