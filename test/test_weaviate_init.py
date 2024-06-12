decd426308f1bbb5fb6b0561885676cb5e3d535d
Nguyen Duong Trung <103947618+ndturng@users.noreply.github.com>
2024-06-12 08:41:53 +0000

    add delete schema test

--- test/test_weaviate_init.py
+++ test/test_weaviate_init.py
@@ -1,11 +1,11 @@
 import requests
 
-from data.weaviate_init import create_schema
+from data.weaviate_init import create_schema, delete_schema
 
 URL = "http://localhost:8080/v1"
 
 
-def test_create_schema():
+def test_create_delete_schema():
     """
     Test create_schema function
     run these command before running the test:
@@ -14,7 +14,11 @@ def test_create_schema():
     """
     schema_name = "ExampleSchema"
     create_schema(schema_name)
-    # # schema ExampleSchema should be exit at URL/schema/ExampleSchema
+    # schema ExampleSchema should be exit at URL/schema/ExampleSchema
     response = requests.get(f"{URL}/schema/{schema_name}")
     assert response.status_code == 200
     
+    # delete schema ExampleSchema
+    delete_schema(schema_name)
+    response = requests.get(f"{URL}/schema/{schema_name}")
+    assert response.status_code == 404
